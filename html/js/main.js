1a58fecba0a675ee4a82b06018da8ed09cd6c1de
saibal-mandal <saibal.freelancer@gmail.com>
2021-06-29 14:18:03 +0530

    upload

---
+++ html/js/main.js
@@ -0,0 +1,170 @@
+//loader
+$(window).load(function() {
+    $('.loader').delay(400).fadeOut(200);
+	});	
+	
+
+//scroll to top	
+$(window).scroll(function(){
+	if($(this).scrollTop()>250){
+		$('.scrollup').fadeIn();
+	}else{
+		$('.scrollup').fadeOut();
+		}
+	});
+$('.scrollup').click(function(){
+	$("html, body").animate({scrollTop:0}, 600);
+	return false;
+	});
+    	
+/*dropdown js*/
+const $dropdown = $(".dropdown");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu");
+const showClass = "show";
+ 
+$(window).on("load resize", function() {
+  if (this.matchMedia("(min-width: 992px)").matches) {
+    $dropdown.hover(
+      function() {
+        const $this = $(this);
+        $this.addClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "true");
+        $this.find($dropdownMenu).addClass(showClass);
+      },
+      function() {
+        const $this = $(this);
+        $this.removeClass(showClass);
+        $this.find($dropdownToggle).attr("aria-expanded", "false");
+        $this.find($dropdownMenu).removeClass(showClass);
+      }
+    );
+  } else {
+    $dropdown.off("mouseenter mouseleave");
+  }
+});
+
+ // owl-carousel banner 
+$('#owldemo1').owlCarousel({
+        loop: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 4500,
+        smartSpeed: 1950,
+        dots: true,
+        animateIn: 'fadeIn',
+        animateOut: 'fadeOut',
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+		
+ // owl-carousel deal 
+$('#owldemo2').owlCarousel({
+        loop: true,
+        nav: true,
+        margin:5,
+        navText : ["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"],
+        autoplay: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 2000,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            575: {
+                items: 1
+            },
+            767: {
+                items: 2
+            },
+            991: {
+                items: 3
+            }
+        }
+    });
+// owl-carousel new product
+$('#owldemo3').owlCarousel({
+        loop: true,
+        nav: true,
+        navText : ["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"],
+        autoplay: true,
+        autoplayTimeout: 4500,
+        smartSpeed: 1950,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1025: {
+                items: 4
+            }
+        }
+    });
+// owl-carousel Popular Products
+$('#owldemo4').owlCarousel({
+        loop: true,
+        nav: true,
+        navText : ["<i class='fas fa-chevron-left'></i>","<i class='fas fa-chevron-right'></i>"],
+        autoplay: true,
+        autoplayTimeout: 6000,
+        smartSpeed: 2550,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1025: {
+                items: 4
+            }
+        }
+    });
+
+// owl-carousel Popular brands
+$('#owldemo5').owlCarousel({
+        loop: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 4500,
+        smartSpeed: 1950,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            500: {
+                items: 3
+            },
+            
+            768: {
+                items: 4
+            },
+            992: {
+                items: 5
+            },
+            1025: {
+                items: 6
+            }
+        }
+    });
